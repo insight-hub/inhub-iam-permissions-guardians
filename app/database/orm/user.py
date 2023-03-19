844eb4273a158329596a3285cb3135bacdec42ff
saymurrmeow <r.savinovskii@gmail.com>
2023-03-20 02:38:01 +0300

    wip: add timestamp func

--- app/database/orm/user.py
+++ app/database/orm/user.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, DateTime, Integer, String
+from sqlalchemy import Boolean, Column, DateTime, Integer, String
+from sqlalchemy.sql import func
 
 from app.database.db import Base
 
@@ -7,7 +8,10 @@ class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, index=True)
-    email = Column(String, index=True)
-    created_at = Column(DateTime)
-    updated_at = Column(DateTime)
+    username = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
+    is_mail_confirmed = Column(Boolean, default=False)
+    hashed_password = Column(String)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True),
+                        default=func.now(), onupdate=func.now())
