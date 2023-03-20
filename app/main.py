39cc582613bcf9bf7fb2817093f0c87b9ed192d7
saymurrmeow <r.savinovskii@gmail.com>
2023-03-21 01:06:09 +0300

    wip: add cors

--- app/main.py
+++ app/main.py
@@ -20,6 +20,7 @@ def get_application() -> FastAPI:
     # TODO
     application.add_middleware(
         CORSMiddleware,
+        allow_origins=['http://localhost:8000'],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
