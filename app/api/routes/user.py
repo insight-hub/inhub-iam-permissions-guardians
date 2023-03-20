9b8f5efc9245be9b1222babc0d41c92c3767cbca
saymurrmeow <r.savinovskii@gmail.com>
2023-03-20 17:10:48 +0300

    wip

--- app/api/routes/user.py
+++ app/api/routes/user.py
@@ -1,3 +1,5 @@
+import json
+from datetime import datetime
 from fastapi import APIRouter, Body, Depends, HTTPException
 from starlette.status import HTTP_400_BAD_REQUEST
 
@@ -5,18 +7,12 @@ from app.api.dependenies.database import get_repository
 from app.database.repositories.user import UserRepository
 from app.models.schemas.user import UserJoin
 from app.utils.auth import check_email_taken, check_username_taken
+from app.utils.otp import get_random_pass
+from app.services import redis
 
 router = APIRouter()
 
 
 @router.post('/join')
 async def create_user(user: UserJoin = Body(), user_repository: UserRepository = Depends(get_repository(UserRepository))):
-    if check_email_taken(user_repository, user.email):
-        return HTTPException(status_code=HTTP_400_BAD_REQUEST, detail="Email already taken")
-
-    if check_username_taken(user_repository, user.username):
-        return HTTPException(status_code=HTTP_400_BAD_REQUEST, detail="Username already taken")
-
-    user = user_repository.create_new_user(user=user)
-
-    return user
+    pass
--- app/models/schemas/rwschema.py
+++ app/models/schemas/rwschema.py
@@ -2,7 +2,5 @@ from app.models.domain.rwmodel import RWModel
 
 
 class RWSchema(RWModel):
-    status: int
-
     class Config(RWModel.Config):
         orm_mode = True
