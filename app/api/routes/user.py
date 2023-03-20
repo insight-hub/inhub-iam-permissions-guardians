1afdd8b32e12884955d599027a342121f2b578eb
saymurrmeow <r.savinovskii@gmail.com>
2023-03-20 03:01:00 +0300

    wip: user repository

--- app/api/routes/user.py
+++ app/api/routes/user.py
@@ -1,11 +1,40 @@
-from fastapi import APIRouter, Depends
-from app.api.dependenies.database import get_repository
+from fastapi import APIRouter, Body, Depends, HTTPException
+from starlette.status import HTTP_400_BAD_REQUEST
 
+from app.api.dependenies.database import get_repository
+from app.database.errors import EntityDoesNotExist
 from app.database.repositories.user import UserRepository
+from app.models.schemas.user import UserJoin
 
 router = APIRouter()
 
 
+def check_username_taken(repo: UserRepository, username: str):
+    try:
+        repo.get_by_username(username=username)
+    except EntityDoesNotExist:
+        return False
+
+    return True
+
+
+def check_email_taken(repo: UserRepository, email: str):
+    try:
+        repo.get_by_email(email=email)
+    except EntityDoesNotExist:
+        return False
+
+    return True
+
+
 @router.post('/join')
-async def create_user(user_repository: UserRepository = Depends(get_repository(UserRepository))):
-    pass
+async def create_user(user: UserJoin = Body(), user_repository: UserRepository = Depends(get_repository(UserRepository))):
+    if check_email_taken(user_repository, user.email):
+        return HTTPException(status_code=HTTP_400_BAD_REQUEST, detail="Email already taken")
+
+    if check_username_taken(user_repository, user.username):
+        return HTTPException(status_code=HTTP_400_BAD_REQUEST, detail="Username already taken")
+
+    user = user_repository.create_new_user(user=user)
+
+    return user
--- app/database/repositories/user.py
+++ app/database/repositories/user.py
@@ -1,7 +1,34 @@
+from app.database.errors import EntityDoesNotExist
 from app.database.repositories.base import BaseRespository
 from app.database.orm.user import User
 from app.models.schemas.user import UserJoin
 
 
 class UserRepository(BaseRespository):
-    pass
+    def get_by_email(self, *, email: str):
+        user_row = self.connection.query(
+            User).filter(User.email == email).first()
+
+        if user_row:
+            return user_row
+
+        raise EntityDoesNotExist
+
+    def get_by_username(self, *, username: str):
+        user_row = self.connection.query(
+            User).filter(User.username == username).first()
+
+        if user_row:
+            return user_row
+
+        raise EntityDoesNotExist
+
+    def create_new_user(self, *, user: UserJoin):
+        db_user = User(username=user.username,
+                       email=user.email, hashed_password="test")
+
+        self.connection.add(db_user)
+        self.connection.commit()
+        self.connection.refresh(db_user)
+
+        return db_user
