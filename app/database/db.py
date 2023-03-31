8bd95e8a3c1b9fedd9c69404edc94acf7f5d5833
saymurrmeow <r.savinovskii@gmail.com>
2023-03-31 07:11:56 +0300

    feat: pre ping db

--- app/database/db.py
+++ app/database/db.py
@@ -5,7 +5,7 @@ from app.core.config import get_app_settings
 
 settings = get_app_settings()
 
-engine = create_engine(settings.pg_url)
+engine = create_engine(settings.pg_url, pool_pre_ping=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
